39383886896d0daaa683081f0143040335ee18d5
Daniel <67976795+Celot1979@users.noreply.github.com>
2021-11-13 01:04:07 +0100

    Calcular los bytes de un directorio

--- com_direc.sh
+++ com_direc.sh
@@ -2,8 +2,18 @@
 #En esté script vamos a ver si existe o no existe un fichero creado
 
 if [ ! -d $1 ]; then
-  echo "Error: El directorio que estás intentando calcular no existe........."
-  fi 
+    echo "Error: el directorio no existe"
+    exit 1
+fi
 
-  name=`ls -l`
-  echo $name
+total=0
+for f in `ls $1`; do
+    name="$1/$f"
+    if [ -f $name ]; then
+        bytes=`ls -l $name | cut -d " " -f 5`
+        echo "Fichero $name ocupa $bytes" | tr -s /
+        (( total = $total + $bytes ))
+    fi
+done
+
+echo -e "\nTotal: $total"
